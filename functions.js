9ecfcd0d95bff8ec860f2c81588d0f927b0c2f04
Bonifase <orwabonifase@gmail.com>
2019-03-06 05:50:17 +0300

    working with arrays

--- functions.js
+++ functions.js
@@ -73,3 +73,12 @@ let arr = ["I", "study", "JavaScript", "right", "now"];
 arr.splice(0, 3, "Let's", "dance");
 
 alert( arr ) // now ["Let's", "dance", "right", "now"]
+
+let arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+alert( arr ); // "I", "study", "complex", "language", "JavaScript"
