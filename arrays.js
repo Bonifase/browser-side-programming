18d47f84505059b5af7343c6a19fc78f0cf3bc52
Bonifase <orwabonifase@gmail.com>
2019-03-06 22:08:41 +0300

    working with forEach

--- arrays.js
+++ arrays.js
@@ -42,3 +42,30 @@ alert( arr.concat(arrayLike) ); // 1,2,something,else
 ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
     alert(`${item} is at index ${index} in ${array}`);
   });
+
+  let range = {
+    from: 1,
+    to: 5
+  };
+  
+  // 1. call to for..of initially calls this
+  range[Symbol.iterator] = function() {
+  
+    // ...it returns the iterator object:
+    // 2. Onward, for..of works only with this iterator, asking it for next values
+    return {
+      current: this.from,
+      last: this.to,
+  
+      // 3. next() is called on each iteration by the for..of loop
+      next() {
+        // 4. it should return the value as an object {done:.., value :...}
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  };
+  
