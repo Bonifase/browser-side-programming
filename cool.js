f94ad39182eac4ad10fe898f8eb20a5f81ed653c
Bonifase <orwabonifase@gmail.com>
2019-03-25 00:19:50 +0300

    working with form inputs

--- cool.js
+++ cool.js
@@ -4,15 +4,15 @@
 // }
 function myFunction(){
     var fruits = [];
-    var name=prompt('Enter the fruit of your choice. Username:',' ');
-    var value=prompt('Enter a value. Please type it now:',' ');
+    var name=document.getElementById("name").value;
+    var value=document.getElementById("value").value;
     var fruit = {};
     fruit['Name'] = name;
     fruit['value'] = value;
     fruits.push(fruit)
 
 
-    document.getElementById("fruit").innerHTML = [...fruits];
+    document.getElementById("fruit").innerHTML = fruit.Name;
     alert(fruits[0].Name);
 }
 
